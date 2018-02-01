9187eca1b1afecb6959d3e57855483cfeb463fb7
ruanpienaar <ruan800@gmail.com>
2018-02-01 14:15:07 +0000

    merge conflicts

--- apps/scrabble/priv/www/js/main.js
+++ apps/scrabble/priv/www/js/main.js
@@ -22,14 +22,131 @@ $(document).ready(function(){
     $('#scrabble_player_tiles').append('<tr>'+squares+'</tr>');
 
     $('.player_tiles').draggable({
-        containment: 'document'
+        containment: 'document',
+        stack: '.game_square',
+        snap: '.game_square',
+        cursor: 'move',
+        stop: function(){
+            alert('dropped');
+        }
     });
 
-    $('.scrabble_board_square').droppable({
-      drop: function( event, ui ) {
-        var dropped = ui.draggable;
-        var droppedOn = $(this);
-        $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);
-      }
-    });
+    var double_letter_score = [
+        4,
+        12,
+        37,
+        39,
+        46,
+        53,
+        60,
+        93,
+        97,
+        99,
+        103,
+        109,
+        117,
+        123,
+        127,
+        129,
+        133,
+        166,
+        173,
+        180,
+        187,
+        189,
+        214,
+        222
+    ];
+    double_word_score = [
+        17,
+        29,
+        33,
+        43,
+        49,
+        57,
+        65,
+        71,
+        155,
+        161,
+        169,
+        177,
+        183,
+        193,
+        197,
+        209
+    ];
+    var tripple_letter_score = [
+        21,
+        25,
+        77,
+        81,
+        85,
+        89,
+        137,
+        141,
+        145,
+        149,
+        201,
+        205
+    ];
+    var tripple_word_score = [
+        1,
+        8,
+        15,
+        106,
+        120,
+        211,
+        218,
+        225
+    ];
+
+    // 113 Center
+
+    // create the scrabble board...
+    //for (var y = -4; y <= 4; y++) {
+    var count = 0;
+    for (var y = 1; y <= 15; y++) {
+        var row='<tr>';
+        //for (var x = -4; x <= 4; x++) {
+        for (var x = 1; x <= 15; x++) {
+            count += 1;
+            var css_class = 'scrabble_square ';
+            var square_text = '&nbsp';
+            var id = 'Y'+y+'X'+x;
+            if( count == 113 ){
+                css_class = 'middle_square';
+            } else if ( double_letter_score.includes(count) ){
+                css_class += ' double_letter_score';
+                square_text = 'DLS';
+            } else if( double_word_score.includes(count) ){
+                css_class += ' double_word_score';
+                square_text = 'DWS';
+            } else if( tripple_letter_score.includes(count) ){
+                css_class += ' tripple_letter_score';
+                square_text = 'TLS';
+            } else if( tripple_word_score.includes(count) ){
+                css_class += ' tripple_word_score';
+                square_text = 'TWS';
+            }
+            row += '<td id="id" class="'+css_class+' game_square">';
+            //row += count;
+            row += square_text;
+            row += '</td>';
+        }
+        $('#scrabble_board').append(row+'</tr>');
+    }
+
+    
+
+    // Once Tile state is resolved, allow play/interaction.
+
 });
+
+// 2 blank tiles (scoring 0 points)
+// 1 point: E ×12, A ×9, I ×9, O ×8, N ×6, R ×6, T ×6, L ×4, S ×4, U ×4
+// 2 points: D ×4, G ×3
+// 3 points: B ×2, C ×2, M ×2, P ×2
+// 4 points: F ×2, H ×2, V ×2, W ×2, Y ×2
+// 5 points: K ×1
+// 8 points: J ×1, X ×1
+// 10 points: Q ×1, Z ×1
